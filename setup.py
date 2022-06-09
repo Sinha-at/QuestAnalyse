aa6cea3a166ccbece76c8bf7e15c7ac1ceb49506
Sinha <99481335+Sinha1111@users.noreply.github.com>
2022-06-10 00:09:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,22 @@ setup(
   author = 'Nina Abittan',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/Sinha1111/QuestAnalyse',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Sinha1111/QuestAnalyse/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['', '', ''],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'pandas',
+          'openpyxl',
+          'pycel',
+          'IPython',
+          'formulas',
+          'numpy',
+          'matplotlib',
+          'ipywidgets',
+          'pathlib',
+          'dataframe_image',
+          'aspose.words',
+          'wordcloud',
+          'scipy',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
