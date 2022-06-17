6a3b9234c2d7ecf619373e5fee346babeedd7b5b
Sinha <99481335+Sinha1111@users.noreply.github.com>
2022-06-17 16:46:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'QuestAnalyse',  
   packages = ['QuestAnalyse'],   
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'allow user to test the questionnaire and compare results between questionnaire',   # short description about my library
   author = 'Nina Abittan', 
